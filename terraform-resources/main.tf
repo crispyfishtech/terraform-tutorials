61213c0736649f0e5d28ffffac1c3cb44725ad90
nayrsirhc <christopher911127@gmail.com>
2025-05-01 06:39:12 +0200

    feat: add depends_on

--- terraform-resources/main.tf
+++ terraform-resources/main.tf
@@ -18,3 +18,21 @@ resource "local_file" "baz" {
   content  = "${each.value}"
   filename = "${path.module}/${each.key}.txt"
 }
+
+resource "random_pet" "file_name" {}
+
+resource "local_file" "pet" {
+  content  = "pet!"
+  filename = "${path.module}/${random_pet.file_name.id}.txt"
+}
+
+resource "local_file" "file1" {
+  content  = "file1!"
+  filename = "${path.module}/mefirst.txt"
+}
+
+resource "local_file" "file2" {
+  content  = "file2!"
+  filename = "${path.module}/mesecond.txt"
+  depends_on = [ local_file.file1 ]
+}
